8ac0ba5efc25ecd42b377f35c27987198414a469
Salzar-R2D2 <129166893+Salzar-R2D2@users.noreply.github.com>
2023-12-24 09:06:22 +0100

    Player_join V2

--- config.lua
+++ config.lua
@@ -1,3 +1,6 @@
+
+------------------ Basic Join/left message ------------------
+
 -- Message when the player joins the server
 
 join_prefix_msg = "[Server] -> "
@@ -13,3 +16,22 @@ left_color_msg = "#00d11d"
 left_message = " left the game."
 
 left_color_player_name = "#00d8ea"
+
+
+------------------ Staff Join/left message ------------------
+
+-- Message when the player joins the server
+
+st_join_prefix_msg = "[Staff] The staff member "
+st_join_color_msg = "#ef1600"
+st_join_msg = " joined the game."
+
+st_join_color_player_name = "#005eef"
+
+-- Message when the player leaves the server
+
+st_left_prefix_msg = "[Staff] The staff member "
+st_left_color_msg = "#ef1600"
+st_left_message = " left the game."
+
+st_left_color_player_name = "#005eef"
--- init.lua
+++ init.lua
@@ -1,21 +1,37 @@
 dofile(minetest.get_modpath("player_join").."/config.lua")
 
+------------------ Basic Join/left message ------------------
+
 -- Join Message
 
 function minetest.send_join_message(player_name)
-	if not core.is_singleplayer() then
-		minetest.chat_send_all(minetest.colorize(join_color_msg, join_prefix_msg).. minetest.colorize(join_color_player_name, player_name ).. minetest.colorize(join_color_msg, join_message))
-		-- minetest.colorize("#005406", "[Server] -> " .. player_name .. " joined the game.")
+	local privs = minetest.get_player_privs(player_name)
+        local staffprivs = privs.admin or privs.moderator or privs.developer or privs.builder 
+        if staffprivs then
+                        minetest.chat_send_all(minetest.colorize(st_join_color_msg, st_join_prefix_msg).. minetest.colorize(st_join_color_player_name, player_name).. minetest.colorize(st_join_color_msg, st_join_msg))
+	else
+			minetest.chat_send_all(minetest.colorize(join_color_msg, join_prefix_msg).. minetest.colorize(join_color_player_name, player_name).. minetest.colorize(join_color_msg, join_message))
 	end
 end
+
 
 -- Left Message
 
 function minetest.send_leave_message(player_name, timed_out)
-	local announcement = minetest.colorize(left_color_msg, left_prefix_msg).. minetest.colorize(left_color_player_name,  player_name).. minetest.colorize(left_color_msg, left_message)
-		if timed_out then
-			announcement = announcement .. " (timed out)"
-		end
-	minetest.chat_send_all(announcement)
-end
+	local privs = minetest.get_player_privs(player_name)
+        local staffprivs = privs.admin or privs.moderator or privs.developer or privs.builder
+        local staff_announcement = minetest.colorize(st_left_color_msg, st_left_prefix_msg).. minetest.colorize(st_left_color_player_name,  player_name).. minetest.colorize(st_left_color_msg, st_left_message)
+ 	local announcement = minetest.colorize(left_color_msg, left_prefix_msg).. minetest.colorize(left_color_player_name,  player_name).. minetest.colorize(left_color_msg, left_message)
+        if staffprivs then
+                if timed_out then
+ 		        staff_announcement = staff_announcement .. " (timed out)"
+ 		end
+ 	minetest.chat_send_all(staff_announcement)
 
+        else
+ 		if timed_out then
+ 			announcement = announcement .. " (timed out)"
+ 		end
+ 	minetest.chat_send_all(announcement)
+   end
+end
